de5ac1daba2284ceb1a4f533fb3063d1b1a681f7
ahmedspurs <amnnn80@gmail.com>
2025-07-25 00:21:08 +0300

    Fix models

--- models/static_sections.js
+++ models/static_sections.js
@@ -36,6 +36,10 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.TEXT,
       allowNull: true
     },
+    file_url: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
     meta_title_ar: {
       type: DataTypes.STRING(255),
       allowNull: true
