d61d0c30253cc3c1a1fc16ffe3f2b31306202421
ahmedspurs <amnnn80@gmail.com>
2025-07-26 18:25:08 +0300

    Old url

--- routes/Cases.js
+++ routes/Cases.js
@@ -17,6 +17,6 @@ router.route("/paginate").post(paginate);
 router.route("/paginate/filtered").post(paginateDashboard);
 router.route("/search").post(search);
 router.route("/by-type").post(byType);
-router.route("/cases/:id").get(getOldCasesById);
+router.route("/old/:id").get(getOldCasesById);
 router.route("/:id").get(getCasesById).put(updateCases).delete(deleteCases);
 module.exports = router;
